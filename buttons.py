1628817d3a54bf19e637a3386ab57f12daed4bf0
Burak ŞEN <buraksenb@gmail.com>
2021-02-26 23:16:26 +0300

    Test Commit

---
+++ buttons.py
@@ -0,0 +1,14 @@
+from tkinter import  *
+
+
+
+root = Tk()
+
+def myClick():
+    myLabel = Label(root,text = "Look! I clicked a Button!")
+    myLabel.pack()
+
+myButton = Button(root,text = "Click me!",command = myClick, fg = "blue", bg = "#00000")
+myButton.pack()
+
+root.mainloop()
